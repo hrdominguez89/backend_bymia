b617550d8054dbd4bb94ef73411b2eb05acebeeb
Hector Ricardo Dominguez <hrdominguez89@gmail.com>
2022-08-01 02:08:50 -0300

    entidad usuario y roles unificada y funcionando

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,16 +12,56 @@ use Doctrine\ORM\Mapping as ORM;
 class User extends BaseUser
 {
     /**
-     * @ORM\Column(type="json")
+     * @ORM\Column(type="string")
      */
     protected $roles = [];
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Roles::class, inversedBy="users")
+     */
+    private $role;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $lastname;
 
     /**
      * @return array
      */
     public function getRoles(): array
     {
-        return array_unique($this->roles);
+        return array_unique([$this->role->getRole()]);
     }
 
+    public function getRole(): ?Roles
+    {
+        return $this->role;
+    }
+
+    public function setRole(?Roles $role): self
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(?string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
 }
