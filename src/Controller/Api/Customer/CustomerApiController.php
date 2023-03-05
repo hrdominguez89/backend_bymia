271e1f68a62aa3d1bae06ad15ae1ba2dc6b6a9d2
Hector Ricardo Dominguez <hrdominguez89@gmail.com>
2023-03-05 18:00:18 -0300

    se resuelve error de header authorization

--- src/Controller/Api/Customer/CustomerApiController.php
+++ src/Controller/Api/Customer/CustomerApiController.php
@@ -31,7 +31,6 @@ class CustomerApiController extends AbstractController
     public function __construct(JWTEncoderInterface $jwtEncoder, CustomerRepository $customerRepository, RequestStack $requestStack)
     {
         $request = $requestStack->getCurrentRequest();
-        dd($request, $request->headers);
         $token = explode(' ', $request->headers->get('Authorization'))[1];
 
         $username = @$jwtEncoder->decode($token)['username'] ?: '';
