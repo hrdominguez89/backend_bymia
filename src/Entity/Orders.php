b56078ee44dd24872cc3f26cccd73c3c9c233305
Hector Ricardo Dominguez <hrdominguez89@gmail.com>
2023-12-01 02:20:02 -0300

    se envia orden al crm

--- src/Entity/Orders.php
+++ src/Entity/Orders.php
@@ -910,10 +910,10 @@ class Orders
             $orders_products_result[] = [
                 'product_id' => $order_product->getProduct()->getId3pl(),
                 'product_name' => $order_product->getProduct()->getName(),
-                'qty' => $order_product->getQuantity(),
-                'weight' => $order_product->getWeight(),
-                'price' => $order_product->getPrice(),
-                'discount' => $order_product->getDiscount()
+                'qty' => $order_product->getProduct()->getQuantity(),
+                'weight' => $order_product->getProduct()->getWeight(),
+                'price' => $order_product->getProduct()->getPrice(),
+                'discount' => $order_product->getProduct()->getDiscount()
             ];
         }
 
