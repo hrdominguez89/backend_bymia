e6226df7176b7c9005fa564aa1139821a1afe9c5
Hector Ricardo Dominguez <hrdominguez89@gmail.com>
2023-12-30 09:36:51 -0300

    se cambia status

--- src/Controller/Api/Customer/CustomerOrderApiController.php
+++ src/Controller/Api/Customer/CustomerOrderApiController.php
@@ -285,7 +285,7 @@ class CustomerOrderApiController extends AbstractController
 
             $transaction = $transactionsRepository->findOneBy($criterios, ['created_at' => 'DESC']);
 
-            if (!$transaction || $transaction->getCreatedAt() >= $fechaActual ) {
+            if (!$transaction || !($transaction->getCreatedAt() >= $fechaActual) ) {
                 $transaction =  new Transactions;
                 $transaction->setNumberOrder($order);
                 $transaction->setStatus($statusTypeTransactionRepository->find(Constants::STATUS_TRANSACTION_NEW));
