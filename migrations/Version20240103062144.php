523ec6bcfbd3fadacd648d15e9797c3e7b7cbc0d
Hector Ricardo Dominguez <hrdominguez89@gmail.com>
2024-01-03 03:24:51 -0300

    se insertan url de tipo de pago

---
+++ migrations/Version20240103062144.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240103062144 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql("UPDATE payment_type set url='https://lab.cardnet.com.do/authorize' WHERE id=2");
+        $this->addSql("UPDATE payment_type set url='https://lab.cardnet.com.do/servicios/pagosxcuotas/BHDPagosCuotas' WHERE id=3");
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+    }
+}
