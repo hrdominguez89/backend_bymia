b8cf29dbed23c841f2568f27f4bff5505de1d1d8
Hector Ricardo Dominguez <hrdominguez89@gmail.com>
2024-01-18 16:47:45 -0300

    se corrige error de repositorio

--- src/Repository/TransactionsRepository.php
+++ src/Repository/TransactionsRepository.php
@@ -46,7 +46,7 @@ class TransactionsRepository extends ServiceEntityRepository
             UPDATE App:Transactions t
             SET
                 t.status = 2,
-                updated_at =:updated_at
+                t.updated_at =:updated_at
             WHERE t.number_order =:number_order
             AND t.status = 1
             ')
