0a0e55755244c9fb1f93146a8d0bcd744e6779cd
Hector Ricardo Dominguez <hrdominguez89@gmail.com>
2024-01-26 19:08:24 -0300

    se sube prueba de carga de proforma

--- src/Controller/Api/Crm/CrmApiOrdersController.php
+++ src/Controller/Api/Crm/CrmApiOrdersController.php
@@ -62,17 +62,6 @@ class CrmApiOrdersController extends AbstractController
                     $body = $request->getContent();
                     $data = json_decode($body, true);
 
-
-                    return $this->json(
-                        [
-                            'data' => $data,
-                            'json_enviado' => json_encode($data),
-                        ],
-                        Response::HTTP_ACCEPTED,
-                        ['Content-Type' => 'application/json']
-                    );
-
-
                     $status_obj = $statusOrderTypeRepository->find($data['status_order']);
 
                     $order->setStatus($status_obj);
@@ -120,22 +109,21 @@ class CrmApiOrdersController extends AbstractController
 
                     $order->setBillFile($data['bill_file'] ?: null);
 
+                    $order->setProformaBillFile($data['proforma_bill_file'] ?: null);
 
-                    // $order->setProformaBillFile($data['proforma_bill_file'] ? $data['proforma_bill_file'] : null);
 
+                    if ($data['payments_files']) {
+                        foreach ($data['payments_files'] as $payment_file) {
+                            $payment_file_obj = $paymentsFilesRepository->findOneBy(['payment_file' => $payment_file]);
+                            if (!$payment_file_obj) {
+                                $payment_file_obj = new PaymentsFiles;
+                                $payment_file_obj->setOrderNumber($order);
+                                $payment_file_obj->setPaymentFile($payment_file);
 
-                    // if ($data['payments_files']) {
-                    //     foreach ($data['payments_files'] as $payment_file) {
-                    //         $payment_file_obj = $paymentsFilesRepository->findOneBy(['payment_file' => $payment_file]);
-                    //         if (!$payment_file_obj) {
-                    //             $payment_file_obj = new PaymentsFiles;
-                    //             $payment_file_obj->setOrderNumber($order);
-                    //             $payment_file_obj->setPaymentFile($payment_file);
-
-                    //             $em->persist($payment_file_obj);
-                    //         }
-                    //     }
-                    // }
+                                $em->persist($payment_file_obj);
+                            }
+                        }
+                    }
 
 
                     if ($data['payments_received_files']) {
