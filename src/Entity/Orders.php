b132bf6f0afad320801a1108654c7a48a69ef584
Hector Ricardo Dominguez <hrdominguez89@gmail.com>
2024-01-27 02:40:19 -0300

    se corrige error de offset

--- src/Entity/Orders.php
+++ src/Entity/Orders.php
@@ -1025,8 +1025,8 @@ class Orders
             "shipping_type" => $this->getShippingType() ? $this->getShippingType()->getId() : null,
             "bill_file" => $this->getBillFile(),
             "proforma_bill_file" => $this->getProformaBillFile(),
-            "payments_files" => $payments_files_result[0],//este y payments_received_files por ahora los dejo asi
-            "payments_received_files" => $payments_files_result?$payments_files_result[0]:[],
+            "payments_files" => $payments_files_result?$payments_files_result[0]:[],//este y payments_received_files por ahora los dejo asi
+            "payments_received_files" => $payments_files_result,
             "payments_transactions_codes" => $payments_transactions_codes_result,
             "debit_credit_notes_files" => $debit_credite_notes_files_result,
             "receiver" => [
