4868dc193ad2e75e171eeb423014534651beb075
Hector Ricardo Dominguez <hrdominguez89@gmail.com>
2024-02-28 00:10:18 -0300

    se hace prueba con cardnet

--- src/Entity/Orders.php
+++ src/Entity/Orders.php
@@ -995,7 +995,7 @@ class Orders
             "order_id" => $this->getId(),
             // "inventory_id" => $this->getInventoryId(),
             "payment_type_id" => $this->getPaymentType() ? $this->getPaymentType()->getId() : null,
-            "cardnet_session" => $this->getTransactions(),
+            "cardnet_session" => $this->getTransactions()[0],
             "cardnet_session_key" => $this->getTransactionApproved() ? $this->getTransactionApproved()->getSessionKey() : null,
             "cardnet_authorization_code" => $this->getTransactionApproved() ? $this->getTransactionApproved()->getAuthorizationCode() : null,
             "cardnet_tx_token" => $this->getTransactionApproved() ? $this->getTransactionApproved()->getTxToken() : null,
