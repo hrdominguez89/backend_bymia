bbec83197c3d5a1d0e7b174d8fd9cc6b33404538
Héctor Ricardo Domínguez <hrdominguez@ambiente.gob.ar>
2024-03-07 00:55:53 -0300

    se sube modificacion en tipo de signo de moneda

---
+++ migrations/Version20240307035317.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240307035317 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql("UPDATE mia_currency set sign ='RD$' WHERE id=1");
+        $this->addSql("UPDATE mia_currency set sign ='US$' WHERE id=2");
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+    }
+}
