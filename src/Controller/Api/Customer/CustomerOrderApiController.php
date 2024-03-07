f104c825b78a1a5dacc4561d419682a1374e37d3
Héctor Ricardo Domínguez <hrdominguez@ambiente.gob.ar>
2024-03-07 05:11:42 -0300

    se corrige configuracion de pagos si hay productos en dolares

--- src/Controller/Api/Customer/CustomerOrderApiController.php
+++ src/Controller/Api/Customer/CustomerOrderApiController.php
@@ -309,7 +309,7 @@ class CustomerOrderApiController extends AbstractController
                 }
             }
 
-            if ($hasProductsInDollars) { //si hay almenos 1 producto en dolares el unico medio de pago es x transferencia
+            if (!$hasProductsInDollars) { //si hay almenos 1 producto en dolares el unico medio de pago es x transferencia
                 $paymentsTypes = $paymentTypeRepository->getPayments();
             } else {
                 $paymentsTypes = $paymentTypeRepository->findBy(['id' => 1]);
